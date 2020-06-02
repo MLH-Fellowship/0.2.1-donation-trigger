519918a5a2d3606de334d89adedb938a451130f8
erin2722 <erin2722@gmail.com>
2020-06-02 16:08:58 -0400

    added basic frontend functiontionality including login button and basic content. next step is adding donation form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  function logIn() {
+    setLoggedIn(true)
+  }
+
   return (
     <div className="App">
-      <h1>donation-bot is coming!</h1>
+      <h1>Welcome to Donation-Bot</h1>
+      <h4>Engagement on social media does matter: Make it matter more by automating your donations based on social media hashtags 
+        and content.</h4>
+      {!loggedIn &&
+        <div className="login" onClick={() => logIn()}>Login To View and Control Your Donations</div>
+      }
+      {loggedIn &&
+        <div className = "content-wrapper">
+          <div className = "welcome-message">
+            <h2>Welcome, USERNAME, here are your selected hashtags, charities, and donation amounts.</h2>
+          </div>
+          <div className = "charities">
+            each of the different charities will go here
+            <div className = "add-charity">+</div>
+          </div>
+        </div>
+      }
     </div>
   );
 }
